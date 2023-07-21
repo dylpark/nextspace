2932677f4fdc63fe5c557992043a617bdfee24ab
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-07-22 08:28:16 +1000

    Creating static blog and user page

---
+++ app/users/page.tsx
@@ -0,0 +1,8 @@
+export default async function Blog() {
+    return (
+        <main className='flex min-h-screen flex-col p-8 lg:p-24'>
+            <h1 className='text-2xl'>Users</h1>
+            <p className='my-2'>...</p>
+        </main>
+    );
+}
