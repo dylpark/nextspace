00fd88606e98205aea6d97d1a0bac99e90d37de5
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-09-12 22:46:08 +1000

    Protecting routes from unauthenticated users

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+export default async function Home() {
+    const session = await getServerSession();
+
+    if (!session) {
+        redirect('/api/auth/signin');
+    }
+
     return <main className='flex min-h-screen flex-col p-8 lg:p-24'>test</main>;
 }
