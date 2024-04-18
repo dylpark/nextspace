95d21cc5a28b7f6090a3eeb84774eb0bcd4bc04e
Dylan Park <64296934+dylpark@users.noreply.github.com>
2024-04-18 23:06:02 +1000

    Fixing user API route fetching

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -9,13 +9,13 @@ interface Props {
 
 export default function UserCard({ id, name, age, image }: Props) {
     return (
-        <div className='bg-white-100 w-[150px] p-0;'>
+        <div className='bg-white-100 p-5 rounded-md border w-full h-full'>
             <img
                 src={image ?? '/mememan.webp'}
                 alt={`${name}'s profile`}
                 className='w-[150px] h-[120px] object-cover mb-2'
             />
-            <div className='px-2 py-0'>
+            <div className='px-2 py-0 text-tertiary'>
                 <h3>
                     <Link href={`/users/${id}`}>{name}</Link>
                 </h3>
